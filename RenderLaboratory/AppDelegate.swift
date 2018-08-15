de94f9c8fd2b6a782e9aaa533519bfa913cfce98
amnesiaice <jxw9391@qq.com>
2018-08-15 22:07:35 +0800

    Initial Commit

---
+++ RenderLaboratory/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RenderLaboratory
+//
+//  Created by Amn Ice on 2018/8/15.
+//  Copyright © 2018 Amn Ice. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RenderLaboratory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RenderLaboratory
+//
+//  Created by Amn Ice on 2018/8/15.
+//  Copyright © 2018 Amn Ice. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
